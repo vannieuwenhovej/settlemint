1d8eee084f18de128f5978e4c456da1d0b9fd62f
vannieuwenhovej <basketjonathan3@hotmail.com>
2021-10-13 21:57:36 +0200

    basic test functionality

--- truffleproject/test/ticket.test.js
+++ truffleproject/test/ticket.test.js
@@ -7,12 +7,31 @@ const Ticket = artifacts.require('./Ticket.sol');
 //     .should();
 
 contract('Ticket', (accounts) => {
+    let contract;
+
+    //ran before every function
+    before(async () => {
+        contract = await Ticket.deployed();
+    })
+
     describe('deployment', async() => { // container for following tests
         it('deploys succesfully', async () => {
-            contract = await Ticket.deployed();
             const address = contract.address;
             assert.notEqual(address, '');
+            assert.notEqual(address, '0x0');
+            assert.notEqual(address, null);
+            assert.notEqual(address, undefined);
             console.log(address);
+        })
+
+        it('has a name', async () => {
+            const name = await contract.name() //this should be returned by the inherited openzeppelin contract
+            assert.equal(name, 'Ticket');
+        })
+
+        it('has a symbol', async () => {
+            const symbol = await contract.symbol()
+            assert.equal(symbol, 'TICKET')
         })
     })
 })
