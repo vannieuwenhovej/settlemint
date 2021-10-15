8c25c95e99f0c84db5d483c5ce56bd45fb4611f1
vannieuwenhovej <basketjonathan3@hotmail.com>
2021-10-15 20:38:37 +0200

    festtoken deployement

--- truffleproject/migrations/2_deploy_contracts.js
+++ truffleproject/migrations/2_deploy_contracts.js
@@ -1,5 +1,7 @@
 const TicketFactory = artifacts.require("TicketFactory");
+const FestToken = artifacts.require("FestToken");
 
 module.exports = function (deployer) {
   deployer.deploy(TicketFactory);
+  deployer.deploy(FestToken);
 };
