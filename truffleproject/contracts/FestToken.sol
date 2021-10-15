e6b924a1086285be897850ff014b55132a4f12a1
vannieuwenhovej <basketjonathan3@hotmail.com>
2021-10-15 20:38:06 +0200

    basic ERC20 functionality for FestToken

--- truffleproject/contracts/FestToken.sol
+++ truffleproject/contracts/FestToken.sol
@@ -1,6 +1,20 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.6.0 <0.9.0;
 
+/**
+@dev compliant with ERC-20 standard: https://github.com/ethereum/EIPs/blob/master/EIPS/eip-20.md
+ */
+
 contract FestToken {
-    
+
+    /**
+    Note: Public variable automatically generates an ERC20 compliant getter function.
+        https://github.com/ethereum/EIPs/blob/master/EIPS/eip-20.md#totalsupply
+     */
+    uint256 public totalSupply;
+  
+    constructor() {
+        totalSupply = 1000000;
+    }
+
 }
