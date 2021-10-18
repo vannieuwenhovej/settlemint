c6b80ec81086340d3874ce380cb0104b8c3f7ddd
vannieuwenhovej <basketjonathan3@hotmail.com>
2021-10-18 23:16:54 +0200

    testing

--- truffleproject/test/festtoken.test.js
+++ truffleproject/test/festtoken.test.js
@@ -4,6 +4,7 @@ const FestToken = artifacts.require('./FestToken.sol');
 
 contract('FestToken', (accounts) => {
     let contract;
+    let [owner, organizer, alice, bob] = accounts;
 
     //ran before every function
     before(async () => {
@@ -21,9 +22,25 @@ contract('FestToken', (accounts) => {
             console.log(address);
         })
         
-        it("should have 1 million as total supply", async () => {
+        it("should have 0 as total supply", async () => {
             let totalSupply = await contract.totalSupply();
-            assert(totalSupply.toNumber(), 1000000);
+            console.log(totalSupply);
+            assert(totalSupply.toNumber(), 0);
+        })
+    })
+
+    describe('ERC20 functionality', async () => {
+        before(async () => {
+        })
+        it('able to mint tokens to user', async () => {
+            await contract.mintTo(organizer, 10000);
+            const balance = await contract.balanceOf(organizer);
+            assert.equal(balance, 10000);
+        })
+        it('should have 10000 as total supply', async () => {
+            let totalSupply = await contract.totalSupply();
+            console.log(totalSupply);
+            assert(totalSupply.toNumber(), 0);
         })
     })
 
--- truffleproject/test/ticket.test.js
+++ truffleproject/test/ticket.test.js
@@ -126,7 +126,7 @@ contract('TicketOwnership', (accounts) => {
             it('alice can buy ticket form organizer', async () => {
                 let price = await contract.getPriceOf(0);
                 await token.allowance(alice, contract.address); //alow contract to spend
-                await token.increaseAllowance(contract.address, price, {from: alice}); //allow contract to spend 
+                await token.increaseAllowance(contract.address, price, {from: alice}); //allow contract to spend price of ticket
                 const result = await contract.buyTicket(0, {from: alice}); //contract spends
                 console.log(result);
                 assert(result.receipt.status, true);
