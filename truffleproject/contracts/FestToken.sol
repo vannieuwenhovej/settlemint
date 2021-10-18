683fcd863f6be5bc510839fb955d67102c823c31
vannieuwenhovej <basketjonathan3@hotmail.com>
2021-10-18 16:33:02 +0200

    FestToken basic functionality

--- truffleproject/contracts/FestToken.sol
+++ truffleproject/contracts/FestToken.sol
@@ -4,17 +4,25 @@ pragma solidity >=0.6.0 <0.9.0;
 /**
 @dev compliant with ERC-20 standard: https://github.com/ethereum/EIPs/blob/master/EIPS/eip-20.md
  */
+import "../node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "../node_modules/@openzeppelin/contracts/access/Ownable.sol";
 
-contract FestToken {
+contract FestToken is ERC20, Ownable{
 
     /**
-    Note: Public variable automatically generates an ERC20 compliant getter function.
-        https://github.com/ethereum/EIPs/blob/master/EIPS/eip-20.md#totalsupply
+    Note: Public variables automatically generates an ERC20 compliant getter function.
+        ex: https://github.com/ethereum/EIPs/blob/master/EIPS/eip-20.md#totalsupply
      */
-    uint256 public totalSupply;
   
-    constructor() {
-        totalSupply = 1000000;
+    /**
+    For now FestToken just uses ERC20 basic functionality
+    */
+
+    constructor(string memory _name, string memory _symbol) ERC20(_name, _symbol) {
+    }
+
+    function mintTo(address _address, uint256 _amount) public onlyOwner() {
+        _mint(_address, _amount);
     }
 
 }
