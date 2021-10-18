5e8a523e67940ed7dbc1124bede1d48e7cc30750
vannieuwenhovej <basketjonathan3@hotmail.com>
2021-10-18 23:16:42 +0200

    constructor parameters set at deployment

--- truffleproject/migrations/2_deploy_contracts.js
+++ truffleproject/migrations/2_deploy_contracts.js
@@ -2,6 +2,16 @@ const TicketOwnership = artifacts.require("TicketOwnership");
 const FestToken = artifacts.require("FestToken");
 
 module.exports = function (deployer) {
-  deployer.deploy(TicketOwnership);
-  deployer.deploy(FestToken);
+  /* Token contract parameters */
+  const _nameOfToken = "FestToken";
+  const _symbolOfToken = "FEST";
+  
+  /* Ticket contract parameters */
+  const _nameOfTicket = "Ticket";
+  const _symbolOfTicket = "TICKET";
+  const _maxSupply = 1000;
+  const _monetization = 0;
+
+  deployer.deploy(FestToken, _nameOfToken, _symbolOfToken);
+  deployer.deploy(TicketOwnership, _nameOfTicket, _symbolOfTicket, _maxSupply, _monetization);
 };
